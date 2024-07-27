d88f0d097cb3de51dc1b496892f18dd6318d24a0
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2024-07-27 09:52:29 -0300

    criando projeto

---
+++ src/main/java/com/gabriel/SantanderBootcamp2024Application.java
@@ -0,0 +1,13 @@
+package com.gabriel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderBootcamp2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderBootcamp2024Application.class, args);
+	}
+
+}
